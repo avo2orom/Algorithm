882cfa79f4c1bd31f2b74368ebe5edba73a9caef
Alex <aov2orom@yandex.ru>
2023-08-20 11:49:54 +0300

    Add 4 task

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,25 @@ public class Main {
     Print.print(array);
     HeapSort.HeapSort(array);
     Print.print(array);
+
+    BinaryTree<Integer> tree = new BinaryTree<>();
+
+    tree.add(6);
+    tree.add(3);
+    tree.add(2);
+    tree.add(4);
+    tree.add(5);
+    tree.add(1);
+    tree.add(9);
+    tree.add(7);
+    tree.add(11);
+    tree.add(8);
+    tree.add(12);
+    tree.add(15);
+    tree.add(10);
+
+    tree.print();
+
+
     }
 }
